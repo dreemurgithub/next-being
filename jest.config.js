eded09c3ced2becfbd4a2f3d968e7e2c5c655cdf
Dat, Le Thanh <lethanhdat1993@gmail.com>
2025-08-31 02:33:55 +0700

    Fix tests using babel, cant start with turbopack

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { presets: ['next/babel', '@babel/preset-react'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
