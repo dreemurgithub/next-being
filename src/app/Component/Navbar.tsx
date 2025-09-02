75835372d1c71481a7d7e6c7bef746ee5427541f
Dat, Le Thanh <lethanhdat1993@gmail.com>
2025-09-03 01:35:58 +0700

    handle logout

--- src/app/Component/Navbar.tsx
+++ src/app/Component/Navbar.tsx
@@ -1,6 +1,27 @@
+'use client'
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/api/auth/logout', {
+        method: 'POST',
+      });
+      if (response.ok) {
+        localStorage.removeItem('jwt')
+        router.push('/login');
+      } else {
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   return (
     <nav className="bg-gray-800 text-white p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
@@ -9,10 +30,12 @@ export default function Navbar() {
             Home
           </Link>
           <Link href="/user" className="hover:text-gray-300">
-            User
+            My Page
           </Link>
         </div>
-        <button className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded">
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded">
           Logout
         </button>
       </div>
